f050e9fb58769764b06cd953973712d334bb4fb4
sebarr90 <arriamarcos@gmail.com>
2024-08-28 17:49:15 -0400

    casi casi listo

---
+++ Assets/js/script.js
@@ -0,0 +1,29 @@
+function mostrarform() {
+    document.getElementById('formu') .style.display = "block";
+}
+$(document).ready(function () {
+    $("#aerovia").click(function () {
+        $("#detallesaerovia").toggle();
+    })
+    $(".btn-close").click(function () {
+        $("#detallesaerovia").hide();
+    });
+
+    $("#leonardo").click(function () {
+        $("#detallesleonardo").toggle();
+    })
+    $(".btn-close").click(function () {
+        $("#detallesleonardo").hide();
+    });
+
+    $("#banco").click(function () {
+        $("#detallesbanco").toggle();
+    })
+    $(".btn-close").click(function () {
+        $("#detallesbanco").hide();
+    });
+    $(".btn-primary") .click(function(){
+        $("#formu").hide();
+    })
+})
+
